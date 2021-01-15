ce420c52f39c658d438cf8e3f26644f8fc2e126b
Christopher Henry <carandoom@gmail.com>
2021-01-15 17:08:57 +0100

    Update Coloc2ChannelsInteractive.m

--- Matlab/Colocalisation/Coloc2ChannelsInteractive_IndividualFunctions/Coloc2ChannelsInteractive.m
+++ Matlab/Colocalisation/Coloc2ChannelsInteractive_IndividualFunctions/Coloc2ChannelsInteractive.m
@@ -1,26 +1,3 @@
-	%	
-	%	Script to get colocalization statistics
-	%	Made to work in batch mode using still images of 2 channels
-	%	You need to select all the images of the first channel
-	%	then all of the second channel. They will be paired by alphabetical order
-	%	You can set the threshold for each channel and apply it
-	%	A potential issue is the image treatment done withing the script,
-	%	it might reduce a lot the signal, if it does so, you will need
-	%	to change/remove the image processing part of the script
-	%	
-	%	Guidelines:
-	%		1) For a given series of images, select all the images from channel 1
-	%		2) To the corresponding series, select all images from channel 2
-	%		3) Both channels of the selected image are displayed, use the
-	%			histograms to set the thresholds
-	%		4) Press the "Apply" button
-	%		5) You can copy paste the values generated in the table
-	%		6) You can close the program or press on the "Next series" button
-	%			to open a new series and repeat the script
-	%   
-	%   Christopher HENRY - V1 - 2020-08-13
-	%
-
 function Coloc2ChannelsInteractive()
 
     close all

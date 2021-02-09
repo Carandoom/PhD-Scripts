e54cf86d91e3f3891160efd1f6818ea029917979
Christopher Henry <carandoom@gmail.com>
2021-02-09 21:35:06 +0100

    Create Sentiment_Analysis.r

---
+++ Ongoing-test/Sentiment_Analysis.r
@@ -0,0 +1,9 @@
+# Script in R
+# 
+# Sentiment Analysis Project in R
+# https://data-flair.training/blogs/data-science-r-sentiment-analysis-project/
+# R help: https://data-flair.training/blogs/r-tutorials-home/
+# R classification: https://data-flair.training/blogs/classification-in-r/
+#
+
+

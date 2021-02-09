5d3702609d192e726eb1776a97791debe908986b
Christopher Henry <carandoom@gmail.com>
2021-02-09 22:01:13 +0100

    Create Wine_Classification.m

---
+++ LearningProjects/Wine_Classification.m
@@ -0,0 +1,10 @@
+% 
+% Matlab
+% 
+% https://www.mathworks.com/help/deeplearning/ug/wine-classification.html
+% Wine classification, use of AI to guess where a wine comes from based
+% on different parameters
+%
+
+
+

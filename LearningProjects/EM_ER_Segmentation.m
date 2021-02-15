402100aacfc0e48042bd18886fab6426c6bd1e35
Christopher Henry <carandoom@gmail.com>
2021-02-15 15:49:47 +0100

    Update and rename EM_Mito_Segmentation.m to EM_ER_Segmentation.m

---
+++ LearningProjects/EM_ER_Segmentation.m
@@ -0,0 +1,30 @@
+% 
+% MatLab
+% 
+% Alpha version of MatLab script to automatically detect endoplasmic reticulum
+% from EM images. A second part would be to add a 3D reconstruction when a stack is provided.
+% 
+% 
+% 
+% 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

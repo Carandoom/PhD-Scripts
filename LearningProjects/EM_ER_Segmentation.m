6a3747b29c5b0b77b74f3f8216083ffcf4d5a107
Christopher Henry <carandoom@gmail.com>
2021-02-15 16:57:08 +0100

    Update EM_ER_Segmentation.m

--- LearningProjects/EM_ER_Segmentation.m
+++ LearningProjects/EM_ER_Segmentation.m
@@ -4,8 +4,9 @@
 % Alpha version of MatLab script to automatically detect endoplasmic reticulum
 % from EM images. A second part would be to add a 3D reconstruction when a stack is provided.
 % 
-% 
-% 
+% It is possible to use some dataset from EMPIAR:
+% https://www.ebi.ac.uk/pdbe/emdb/empiar/
+% https://www.ebi.ac.uk/pdbe/emdb/empiar/entry/10592/
 % 
 
 

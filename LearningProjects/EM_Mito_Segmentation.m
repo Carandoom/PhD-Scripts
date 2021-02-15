812a2a6175157c58a9d9ec6fe4c654033af5e687
Christopher Henry <carandoom@gmail.com>
2021-02-15 15:45:21 +0100

    Update EM_Mito_Segmentation.m

--- LearningProjects/EM_Mito_Segmentation.m
+++ LearningProjects/EM_Mito_Segmentation.m
@@ -2,8 +2,8 @@
 % MatLab
 % 
 % Alpha version of MatLab script to automatically detect mitochondria
-% from EM images and do a 3D reconstruction of the mitochondria network
-% 
+% from EM images 
+% A second part would be to add a 3D reconstruction when a stack is provided
 % 
 % 
 % 

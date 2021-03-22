99f3ccb9be0fde127ff2fbb428e1342fd577ac3a
Christopher Henry <carandoom@gmail.com>
2021-03-22 11:56:11 +0100

    Update SOCE_Data_Generator.py

--- LearningProjects/SOCE_Data_Generator.py
+++ LearningProjects/SOCE_Data_Generator.py
@@ -8,17 +8,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # set number of time points and number of cells
-x = 100
+x = 800
 y = 25
 
 # create the different array to store data in
-a = np.random.randint(0.3,0.5, size=(round(x/5),y))
-b = np.random.randint(0.5,0.7, size=(round(x/5),y))
-c = np.random.randint(0.65,0.9, size=(round(x/5),y))
-d = np.random.randint(0.9,1.5, size=(round(x/5),y))
-e = np.random.randint(1.45,1.55, size=(round(x/5),y))
-SOCE = np.concatenate(a,b,c,d,e, axis=0)
+a = np.random.randint(3,15, size=(round(x/10),y))
+b = np.random.randint(12,25, size=(round(x/25),y))
+c = np.random.randint(20,55, size=(round(x/25),y))
+d = np.random.randint(50,70, size=(round(x/20),y))
+e = np.random.randint(65,72, size=(round(x/10),y))
+SOCE = np.concatenate((a,b,c,d,e), axis=0)
 x = SOCE.shape[0]
 
 # plot the SOCE trace corresponding to cell 3
-plt.plot(x, SOCE)
+plt.plot(SOCE[:,3], 'ro')
